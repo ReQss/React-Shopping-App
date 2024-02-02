8b05219fc0ba00b839bfe480a787bd3c7ad9ba1c
ReQss <ReQsPL@gmail.com>
2024-02-02 15:53:45 +0100

    commit main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header'
+import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <Home />
     </div>
   );
